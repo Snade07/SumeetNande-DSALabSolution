119ecbbd2b6c1d3462f784c80dec1f5b5fd875c9
Snade07 <99168242+Snade07@users.noreply.github.com>
2022-05-02 13:53:23 +0530

    Add files via upload

---
+++ Ques1/Bracket.java
@@ -0,0 +1,31 @@
+package Ques1;
+
+public class Bracket {
+    private Character openBracket;
+    private Character closeBracket;
+
+    public Bracket(
+            Character openBracket,
+            Character closeBracket) {
+
+        this.openBracket = openBracket;
+        this.closeBracket = closeBracket;
+    }
+
+    public Character getOpenBracket() {
+        return openBracket;
+    }
+
+    public void setOpenBracket(Character openBracket) {
+        this.openBracket = openBracket;
+    }
+
+    public Character getCloseBracket() {
+        return closeBracket;
+    }
+
+    public void setCloseBracket(Character closeBracket) {
+        this.closeBracket = closeBracket;
+    }
+
+}
---
+++ Ques1/BracketsManager.java
@@ -0,0 +1,51 @@
+package Ques1;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class BracketsManager {
+
+    private static Set<Bracket> predefinedBrackets;
+
+    static {
+        predefinedBrackets = new LinkedHashSet<Bracket>();
+
+        predefinedBrackets.add(new Bracket('(', ')'));
+        predefinedBrackets.add(new Bracket('[', ']'));
+        predefinedBrackets.add(new Bracket('{', '}'));
+        predefinedBrackets.add(new Bracket('<', '>'));
+
+    }
+        public static Set<Character> getOpenBrackets(){
+
+        Set<Character> result = new LinkedHashSet<Character>();
+            Iterator<Bracket> iterator = predefinedBrackets.iterator();
+            while (iterator.hasNext()){
+                Bracket bracket = iterator.next();
+                result.add(bracket.getOpenBracket());
+            }
+            return result;
+        }
+    public static Set<Character> getCloseBrackets(){
+
+        Set<Character> result = new LinkedHashSet<Character>();
+        Iterator<Bracket> iterator = predefinedBrackets.iterator();
+        while (iterator.hasNext()){
+            Bracket bracket = iterator.next();
+            result.add(bracket.getCloseBracket());
+        }
+        return result;
+    }
+    public static Bracket getBracket(Character closeBracket) {
+
+        for (Bracket predefinedBracket : predefinedBrackets) {
+
+            if (predefinedBracket.getCloseBracket().equals(
+                    closeBracket)) {
+                return predefinedBracket;
+            }
+        }
+        return null;
+    }
+}
